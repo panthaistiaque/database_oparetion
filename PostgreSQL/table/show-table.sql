b0a3e7d0d0b4f784bdaba4c01b892934844bf2f5
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-07-31 11:53:58 +0600

    show table command

--- PostgreSQL/table/show-table.sql
+++ PostgreSQL/table/show-table.sql
@@ -1,5 +1,5 @@
---for get this column value
--- schemaname,	tablename,	tableowner,	"tablespace",	hasindexes,	hasrules,	hastriggers,	rowsecurity
+-- for get this column value
+-- schemaname, tablename, tableowner, tablespace, hasindexes, hasrules, hastriggers, rowsecurity
 		
 SELECT * FROM pg_catalog.pg_tables  
 WHERE schemaname != 'pg_catalog'  
@@ -7,9 +7,9 @@ AND schemaname != 'information_schema';
 
 --- OR ------
 
---for get this column value
---table_catalog, table_schema, table_name, table_type, self_referencing_column_name, reference_generation, 
---user_defined_type_catalog, user_defined_type_schema,user_defined_type_name, is_insertable_into, is_typed,	commit_action
+-- for get this column value
+-- table_catalog, table_schema, table_name, table_type, self_referencing_column_name, reference_generation, 
+-- user_defined_type_catalog, user_defined_type_schema,user_defined_type_name, is_insertable_into, is_typed, commit_action
 
 select *   
 from information_schema.tables   
