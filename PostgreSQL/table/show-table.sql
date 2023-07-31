856e4843fc2ddf03b3a49d12dfa3f779248c3a13
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-07-31 11:21:50 +0600

    show table command update

--- PostgreSQL/table/show-table.sql
+++ PostgreSQL/table/show-table.sql
@@ -8,7 +8,8 @@ AND schemaname != 'information_schema';
 --- OR ------
 
 --for get this column value
---table_catalog,	table_schema,	table_name,	table_type,	self_referencing_column_name,	reference_generation,	user_defined_type_catalog,	user_defined_type_schema,	user_defined_type_name,	is_insertable_into,	is_typed,	commit_action
+--table_catalog, table_schema, table_name, table_type, self_referencing_column_name, reference_generation, 
+--user_defined_type_catalog, user_defined_type_schema,user_defined_type_name, is_insertable_into, is_typed,	commit_action
 
 select *   
 from information_schema.tables   
