d1a8864ff31e4405c0afd89562fa9fff1bd3fd2c
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-07-31 11:16:44 +0600

    table query

---
+++ PostgreSQL/table/show-table.sql
@@ -0,0 +1,15 @@
+--for get this column value
+-- schemaname,	tablename,	tableowner,	"tablespace",	hasindexes,	hasrules,	hastriggers,	rowsecurity
+		
+SELECT * FROM pg_catalog.pg_tables  
+WHERE schemaname != 'pg_catalog'  
+AND schemaname != 'information_schema'; 
+
+--- OR ------
+
+--for get this column value
+--table_catalog,	table_schema,	table_name,	table_type,	self_referencing_column_name,	reference_generation,	user_defined_type_catalog,	user_defined_type_schema,	user_defined_type_name,	is_insertable_into,	is_typed,	commit_action
+
+select *   
+from information_schema.tables   
+where table_schema='ehos';  
