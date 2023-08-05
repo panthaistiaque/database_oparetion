ed97c17b66d2dfc82288897e549149a6004c6811
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-08-05 12:56:38 +0600

    Queries::delete

---
+++ PostgreSQL/Queries/delete.sql
@@ -0,0 +1,16 @@
+DELETE FROM department  
+WHERE dept_id = 6; 
+
+-- DELETE USING command
+DELETE FROM department   
+USING department_tmp  
+WHERE  
+department.dept_id = department_tmp.dept_id; 
+
+
+DELETE FROM department  
+WHERE dept_id = (SELECT dept_id FROM department_tmp); 
+
+-- DELETE and returning the removed rows
+    DELETE FROM department_tmp   
+    RETURNING *;  
