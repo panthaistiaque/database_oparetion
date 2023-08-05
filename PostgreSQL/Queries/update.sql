e13a668d72645138c7884c4866bcef60663d0d78
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-08-05 12:24:41 +0600

    Queries::update

--- PostgreSQL/Queries/update.sql
+++ PostgreSQL/Queries/update.sql
@@ -1,21 +1,16 @@
 -- update joins command
 UPDATE department_tmp  
-SET location = department.location,  
-description = department.description,  
-last_update = department.last_update  
-FROM  
-department  
-WHERE  
-department_tmp.Dept_id = department.Dept_id; 
+	SET location = department.location,  
+	description = department.description,  
+	last_update = department.last_update  
+FROM  department  
+WHERE  department_tmp.Dept_id = department.Dept_id; 
 
 
 -- Update command through returning condition
 
-    UPDATE department  
+UPDATE department  
     SET description = 'Names of departments',  
     location = 'NewYork'  
-    WHERE  
-    dept_id = 1   
-    RETURNING dept_id,  
-    description,  
-    location;  
+WHERE  dept_id = 1   
+RETURNING dept_id, description, location;  
