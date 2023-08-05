34db347eb7bc2694a414b411128d15ec2f9f8459
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-08-05 13:06:26 +0600

    Queries::delete

--- PostgreSQL/Queries/delete.sql
+++ PostgreSQL/Queries/delete.sql
@@ -4,13 +4,17 @@ WHERE dept_id = 6;
 -- DELETE USING command
 DELETE FROM department   
 USING department_tmp  
-WHERE  
-department.dept_id = department_tmp.dept_id; 
+WHERE  department.dept_id = department_tmp.dept_id; 
 
 
 DELETE FROM department  
 WHERE dept_id = (SELECT dept_id FROM department_tmp); 
 
 -- DELETE and returning the removed rows
-    DELETE FROM department_tmp   
-    RETURNING *;  
+DELETE FROM department_tmp   
+RETURNING *;  
+
+
+ delete from patient 
+ where patient_code = 'HH0100013845'
+ returning patient.*;
