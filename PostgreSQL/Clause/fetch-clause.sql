1a5a5528a73c5fa231ff1a31cb1fc363ddc61222
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-08-07 00:04:10 +0600

    Clause:: fetch-clause

--- PostgreSQL/Clause/fetch-clause.sql
+++ PostgreSQL/Clause/fetch-clause.sql
@@ -4,3 +4,19 @@ SELECT car_id, car_name, car_model
 FROM Car  
 ORDER BY car_name   
 FETCH FIRST ROW ONLY; 
+
+
+
+SELECT car_id, car_name, car_model  
+FROM Car  
+ORDER BY car_name   
+FETCH first 1 ROW ONLY;  
+
+
+
+
+SELECT car_id, car_name, car_model  
+FROM Car  
+ORDER BY car_name  
+OFFSET 4 ROWS   
+FETCH first 4 ROW ONLY;  
