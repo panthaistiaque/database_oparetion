88aba70d9bcb3bf84da51450f610a7d14e89ceb8
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-08-06 09:11:10 +0600

    Clause:: limit

---
+++ PostgreSQL/Clause/limit.sql
@@ -0,0 +1,12 @@
+-- getting the top and bottom rows using LIMIT Clause
+SELECT Car_id, Car_name, Car_Price  
+FROM CAR  
+ORDER BY Car_Price desc  
+LIMIT 5 ; 
+
+-- OFFSET using PostgreSQL LIMIT clause
+
+SELECT Car_id, Car_name, Body_Style  
+FROM CAR  
+ORDER BY Car_id  
+LIMIT 5 OFFSET 2;
