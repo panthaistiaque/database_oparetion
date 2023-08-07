88869e5a255255f7c5d9f4fa4ffcb4862012d239
panthaistiaque <33906293+panthaistiaque@users.noreply.github.com>
2023-08-07 14:31:34 +0600

    sp for service delete (update)

--- PostgreSQL/procedure/delete_service.sql
+++ PostgreSQL/procedure/delete_service.sql
@@ -4,6 +4,32 @@ create or replace procedure delete_service(
 language plpgsql    
 as $$
 begin
+
+	-- -- -- -- -- -- -- 
+	delete from service_execution_medication sem 
+	where sem.service_id = (
+		select sm.service_id from service_master sm where sm.service_code = s_code
+	);
+	-- -- -- -- -- -- -- 
+	delete from service_diagnosis sd 
+	where sd.service_id = (
+		select sm.service_id from service_master sm where sm.service_code = s_code
+	);
+	-- -- -- -- -- -- -- 
+	delete from service_investigation si  
+	where si.service_id = (
+		select sm.service_id from service_master sm where sm.service_code = s_code
+	);
+	-- -- -- -- -- -- -- 
+	delete from service_execution_details sed 
+	where sed.service_id = (
+		select sm.service_id from service_master sm where sm.service_code = s_code
+	);
+	-- -- -- --- -- -- - - - 
+	delete from service_execution se 
+	where se.service_id = (
+		select sm.service_id from service_master sm where sm.service_code = s_code
+	);
 	-- delete  refund_dtl
     delete from refund_dtl rd 
 	where rd.refund_id in (
@@ -30,3 +56,7 @@ begin
 
     commit;
 end;$$
+
+
+--call delete_service('HH0900033124');
+--drop procedure delete_service(varchar(20));
